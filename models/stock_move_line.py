ba2545349d88df469b317916024db16b7e7700b3
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-02-26 22:20:29 +0100

    Stock move line:Improve code clearness

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -7,15 +7,15 @@ from odoo.exceptions import ValidationError
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
+
     def _action_done(self):
-        try:
-            picking = self.mapped('picking_id')[0]
-        except IndexError as ie:
-            return super(StockMoveLine, self)._action_done()
-        if picking.picking_type_id.code == 'incoming' and picking.purchase_id:
+        # The assumption here is the all move lines comes from the same object (picking,mrp_production)
+        picking = self.mapped('picking_id')
+        if picking and picking.picking_type_id.code == 'incoming' and picking.purchase_id:
             return super(StockMoveLine, self.with_context(purchase_order_id=picking.purchase_id))._action_done()
         else:
             return super(StockMoveLine, self)._action_done()
+
 
 
     
